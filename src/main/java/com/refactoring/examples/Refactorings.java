330bbd62a4638fb1925406da558eb5f0e5afaeeb
Romulo <romulo8000@gmail.com>
2018-01-10 21:07:39 -0200

    Added code for refactoring catalog

---
+++ src/main/java/com/refactoring/examples/Refactorings.java
@@ -0,0 +1,12 @@
+package com.refactoring;
+
+import com.refactoring.beans.*;
+
+public class Refactorings {
+
+        private static List<RegularPrice>
+
+        void printOwing() {
+
+        }
+}
